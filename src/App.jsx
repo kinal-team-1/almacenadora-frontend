5fb454a765cc92f815ba4ba0152e11a9918a3fdf
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-04-26 21:50:05 -0600

    Integrating eslint and prettier config, also, setting up basic CI and CD

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
-    return (
-        <div className="bg-red-400 h-screen">
-            <h1>React App</h1>
-        </div>
-    );
+  return (
+    <div className="bg-red-400 h-screen">
+      <h1>React App</h1>
+    </div>
+  );
 }
