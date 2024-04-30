e5a2b17039affe07495a2d982ed8ce2e88b6f73f
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-04-30 14:04:00 -0600

    AL-4 #comment arreglos del task en diseno y botones #time 20m

--- src/components/FormAddTask.jsx
+++ src/components/FormAddTask.jsx
@@ -17,14 +17,28 @@ export default function NewNoteModal() {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleCancel = () => {
+    
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-500 bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-xs" >
-        <h2 className="text-xl font-semibold mb-1">New Note</h2>
+      <div
+        className="bg-white p-6 rounded-lg shadow-lg"
+        style={{
+          width: "500px",
+          height: "650px",
+          top: "118px",
+          left: "450px",
+          borderRadius: "16px",
+        }}
+      >
+        {" "}
+        <h2 className="text-xl font-semibold mb-1 text-center">New Note</h2>
         <form onSubmit={handleSubmit} className="space-y-5">
           <input
             type="text"
@@ -32,7 +46,7 @@ export default function NewNoteModal() {
             placeholder="Start Date (YYYY-MM-DD)"
             value={formData.startDate}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full"
           />
           <input
             type="text"
@@ -40,7 +54,7 @@ export default function NewNoteModal() {
             placeholder="End Date (YYYY-MM-DD)"
             value={formData.endDate}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full"
           />
           <input
             type="text"
@@ -48,20 +62,20 @@ export default function NewNoteModal() {
             placeholder="Task Name"
             value={formData.taskName}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full"
           />
           <textarea
             name="description"
             placeholder="Description"
             value={formData.description}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full"
           />
           <select
             name="status"
             value={formData.status}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full"
           >
             <option value="">Select Status</option>
             <option value="TODO">TODO</option>
@@ -74,7 +88,7 @@ export default function NewNoteModal() {
             placeholder="Label"
             value={formData.label}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full"
           />
           <input
             type="text"
@@ -82,7 +96,7 @@ export default function NewNoteModal() {
             placeholder="First Name"
             value={formData.firstName}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full"
           />
           <input
             type="text"
@@ -90,11 +104,23 @@ export default function NewNoteModal() {
             placeholder="Last Name"
             value={formData.lastName}
             onChange={handleChange}
-            className="border border-gray-300 rounded px-4 py-2 w-full"
+            className="border border-indigo-600 rounded px-4 py-2 w-full "
           />
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Submit
-          </button>
+          <div className="flex gap-3 w-full justify-between">
+            <button
+              type="submit"
+              className="bg-indigo-600 text-white px-3 py-2 rounded flex gap-3 items-center"
+            >
+              Submit
+            </button>
+            <button
+              type="submit"
+              className="bg-indigo-600 text-white px-3 py-2 rounded flex gap-3 items-center"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
         </form>
       </div>
     </div>
